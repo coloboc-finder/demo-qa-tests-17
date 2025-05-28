75e60ad5f39b66920a7269024b46c34ec0297b12
coloboc-finder <gk120303@gmail.com>
2025-05-28 13:46:37 +0500

    added build.gradle added JUnitTest.java

---
+++ src/test/java/junit/JUnitTest.java
@@ -0,0 +1,35 @@
+package junit;
+
+import org.junit.jupiter.api.*;
+
+import java.lang.module.Configuration;
+
+public class JUnitTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration
+    }
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("        Это метод BeforeEach!");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("        Это метод AfterEach!");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("            Это тест №1!");
+        Assertions.assertTrue(3>2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("            Это тест №2!");
+        Assertions.assertTrue(3>2);
+    }
+
+}
