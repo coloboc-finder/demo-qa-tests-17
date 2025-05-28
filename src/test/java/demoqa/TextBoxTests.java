aa143f0eeb4e84a3d0e1f77fa19c632b47792b08
coloboc-finder <gk120303@gmail.com>
2025-05-28 17:20:50 +0500

    added fillFromTests

--- src/test/java/demoqa/TextBoxTests.java
+++ src/test/java/demoqa/TextBoxTests.java
@@ -10,13 +10,13 @@ public class TextBoxTests {
 
     @Test
     void fillFormTest() {
-//        open("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/");
-//        $("button[ng-click='manager()']").click();
-//        $("button[ng-click='addCust()']").click();
-//        $("input[ng-model='fName']").setValue("Ivan");
-//        $("input[ng-model='lName']").setValue("Ivanov");
-//        $("input[ng-model='postCd']").setValue("123456");
-//        $("button[type='submit']").click();
+        open("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/");
+        $("button[ng-click='manager()']").click();
+        $("button[ng-click='addCust()']").click();
+        $("input[ng-model='fName']").setValue("Ivan");
+        $("input[ng-model='lName']").setValue("Ivanov");
+        $("input[ng-model='postCd']").setValue("123456");
+        $("button[type='submit']").click();
 //
 //        open("https://demoqa.com/text-box");
 //        // Ожидаем, что заголовок страницы будет видим
